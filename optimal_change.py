9b0881fb468dad5739e08fd15f05b02bac7f3da2
madisonelissa <madisonefreese@gmail.com>
2023-06-11 16:26:10 -0700

    optimal change solution in python

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,49 @@
-# Write your solution here!
+def optimal_change(item_cost, amount_paid):
+    change_amounts = {
+        '$100' : 100.00,
+        '$50': 50.00,
+        '$20': 20.00,
+        '$10': 10.00,
+        '$5': 5.00,
+        '$1': 1.00,
+        'quarter': 0.25,
+        'dime': 0.10,
+        'nickel': 0.05,
+        'penny': 0.01
+    }
+    counts = {}
+    
+    #Set res value with initial item cost and amount paid
+    res = f'The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is '
+    
+    #Create a dict of counts for each change amount, from highest to lowest monetary value
+    change = amount_paid - item_cost
+    for key, value in change_amounts.items():
+        while change >= value:
+            counts[key] = counts.get(key, 0) + 1
+            change = round(change, 2) - value
+    
+    #If exact change is given, no change returned
+    if counts == {}:
+        return "No change necessary."
+
+    #Organize and format res string
+    for i, (key, value) in enumerate(counts.items()):
+        if value > 1 and '$' in key:
+            res = res + f'{value} {key} bills'
+        elif value == 1 and '$' in key:
+            res = res + f'{value} {key} bill'
+        elif value > 1 and key != 'penny':
+            res = res + f'{value} {key}s'
+        elif value > 1 and key == 'penny':
+            res = res + f'{value} pennies'
+        elif value == 1:
+            res = res + f'{value} {key}'
+        
+        #Add commas between values. If this iteration is the last, add a period to the end of the sentence
+        if i == len(counts) - 1:
+            res = res + '.'
+        else:
+            res = res + ', '
+            
+    return res
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,33 @@
-# Write your unit tests here!
+from optimal_change import optimal_change
+import unittest
+
+class change(unittest.TestCase):
+    def test_func(self):
+        pass
+    
+    def test_returns_val(self):
+        """Tests func returns value"""
+        self.assertIsNotNone(optimal_change(0, 0))
+        
+    def test_change1(self):
+        """Tests func returns correct change on test value 1"""
+        self.assertEqual(optimal_change(62.13, 100), 'The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, 2 pennies.')
+        
+    def test_change2(self):
+        """Tests func returns correct change on test value 2"""
+        self.assertEqual(optimal_change(31.51, 50), 'The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, 4 pennies.')
+        
+    def test_change3(self):
+         """Tests func returns "No change necessary" on test value 3"""
+         self.assertEqual(optimal_change(100.25, 100.25), 'No change necessary.')
+         
+    def test_change3(self):
+         """Tests func returns correct change on test value 4"""
+         self.assertEqual(optimal_change(8.56, 10), 'The optimal change for an item that costs $8.56 with an amount paid of $10 is 1 $1 bill, 1 quarter, 1 dime, 1 nickel, 4 pennies.')
+         
+    def test_change3(self):
+         """Tests func returns correct change on test value 5"""
+         self.assertEqual(optimal_change(1.25, 2), 'The optimal change for an item that costs $1.25 with an amount paid of $2 is 3 quarters.')
+
+if __name__ == "__main__":
+    unittest.main()
